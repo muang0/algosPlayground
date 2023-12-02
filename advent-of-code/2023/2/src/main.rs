9a316db33cdf5dbfab18965c3d1c1bb70af0e702
muang0 <meanderani@gmail.com>
2023-12-02 14:50:43 -0500

    advent-of-code day 2 part 2

--- advent-of-code/2023/2/src/main.rs
+++ advent-of-code/2023/2/src/main.rs
@@ -1,30 +1,25 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
-    let mut game_sum: u32 = 0;
+    let mut game_sum: i32 = 0;
     let game_doc = fs::read_to_string("./data/input.txt").unwrap();
     for line in game_doc.split("\n") {
-        let game_id = line.split(" ").nth(1).unwrap().split(":").nth(0).unwrap().to_string().parse::<u32>().unwrap();
-        let mut game_is_possible = true;
+        let mut min_cubes = HashMap::new();
+        min_cubes.insert("red", 0);
+        min_cubes.insert("blue", 0);
+        min_cubes.insert("green", 0);
         for round in line.split(":").nth(1).unwrap().split(";") {
             for cube_set in round.split(",") {
                 let num_cubes = cube_set.split(" ").nth(1).unwrap().to_string().parse::<i32>().unwrap();
                 let cubes_color = cube_set.split(" ").nth(2).unwrap();
-                let max_cubes_allowed = match cubes_color {
-                    "red" => 12,
-                    "green" => 13,
-                    "blue" => 14,
-                    _ => panic!("unknown color encountered")
-                };
-                if num_cubes > max_cubes_allowed {
-                    game_is_possible = false;
-                    break;
+                if *min_cubes.get(cubes_color).unwrap() < num_cubes {
+                    min_cubes.insert(cubes_color, num_cubes);
                 }
             }
         }
-        if game_is_possible {
-            game_sum += game_id;
-        }
+        let round_power = min_cubes.get("red").unwrap() * min_cubes.get("green").unwrap() * min_cubes.get("blue").unwrap();
+        game_sum += round_power;
     }
     println!("Game sum: {:#}", game_sum);
 }
