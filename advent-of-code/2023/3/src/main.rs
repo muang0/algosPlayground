f1c47950c571cba69224ae1dc8ec1b741aec5b2a
muang0 <meanderani@gmail.com>
2023-12-03 17:37:19 -0500

    advent-of-code day 3 part 2

--- advent-of-code/2023/3/src/main.rs
+++ advent-of-code/2023/3/src/main.rs
@@ -15,10 +15,8 @@ impl Part {
     }
 }
 
-// schematicSymbols: hashmap<hashmap><bool>
-
 fn main() {
-    let mut parts_sum: u32 = 0;
+    let mut gear_sum: u32 = 0;
     let mut schematic_symbols: HashMap<u32, HashMap<u32, bool>> = HashMap::new();
     let mut parts: Vec<Part> = vec![];
     let engine_schematic = fs::read_to_string("./data/input.txt").unwrap();
@@ -37,13 +35,19 @@ fn main() {
                 part.number.push(c);
                 part.location
                     .push([y_index.try_into().unwrap(), x_index.try_into().unwrap()]);
-            } else {
+            } else if c == '*' {
                 // if full part has been read, push to parts vec
                 if !part.number.is_empty() {
                     parts.push(part);
                     part = Part::new();
                 }
                 line_schematic_symbols.insert(x_index.try_into().unwrap(), true);
+            } else {
+                // if full part has been read, push to parts vec
+                if !part.number.is_empty() {
+                    parts.push(part);
+                    part = Part::new();
+                }
             }
         }
         // if full part has been read, push to parts vec
@@ -55,39 +59,29 @@ fn main() {
         }
     }
     // for each part, determine if it should be included in the sum by searching for adjacent schematicSymbols
-    for part in parts {
-        let mut is_next_to_symbol = false;
-        'outer: for index in part.location {
-            for [y_index, x_index] in [[0, 1], [1, 0], [1, 1]] {
-                if let Some(line_map) = schematic_symbols.get(&index[0].wrapping_add(y_index)) {
-                    if line_map.get(&index[1].wrapping_add(x_index)) == Some(&true) {
-                        is_next_to_symbol = true;
-                        break 'outer;
-                    }
-                }
-                if let Some(line_map) = schematic_symbols.get(&index[0].wrapping_sub(y_index)) {
-                    if line_map.get(&index[1].wrapping_sub(x_index)) == Some(&true) {
-                        is_next_to_symbol = true;
-                        break 'outer;
-                    }
-                }
-                if let Some(line_map) = schematic_symbols.get(&index[0].wrapping_add(y_index)) {
-                    if line_map.get(&index[1].wrapping_sub(x_index)) == Some(&true) {
-                        is_next_to_symbol = true;
-                        break 'outer;
-                    }
-                }
-                if let Some(line_map) = schematic_symbols.get(&index[0].wrapping_sub(y_index)) {
-                    if line_map.get(&index[1].wrapping_add(x_index)) == Some(&true) {
-                        is_next_to_symbol = true;
-                        break 'outer;
+    let mut gears: HashMap<u32, HashMap<u32, Vec<String>>> = HashMap::new();
+    // initalize gears hashmap
+    for (y_index, _) in engine_schematic.split("\n").into_iter().enumerate() {
+        gears.insert(y_index.try_into().unwrap(), HashMap::new());
+    }
+    // for each gear, find all adjacent parts
+    for (y_index_gear, x_index_map) in schematic_symbols {
+        for (x_index_gear, _) in x_index_map {
+            let mut adjacent_gears: Vec<String> = Vec::new();
+            for part in parts.iter() {
+                for [y_index_part, x_index_part] in part.location.iter() {
+                    // if part is within one unit of gear in any direction, add to gear vec
+                    if (y_index_gear as i64 - *y_index_part as i64).abs() <= 1 && (x_index_gear as i64 - *x_index_part as i64).abs() <= 1 {
+                        adjacent_gears.push(part.number.clone());
+                        break
                     }
                 }
             }
-        }
-        if is_next_to_symbol {
-            parts_sum += part.number.parse::<u32>().unwrap();
+            // if only two parts adjacent to the gear, add to sum
+            if adjacent_gears.len() == 2 {
+                gear_sum += adjacent_gears.get(0).unwrap().parse::<u32>().unwrap() * adjacent_gears.get(1).unwrap().parse::<u32>().unwrap()
+            }
         }
     }
-    println!("Parts sum: {:#}", parts_sum);
+    println!("gear sum: {:#}", gear_sum);
 }
