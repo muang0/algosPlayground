fb66d697e68f24dd252daa47a78cf73045b8c96c
muang0 <meanderani@gmail.com>
2023-12-07 11:55:21 -0500

    advent-of-code day 5 part 2

--- advent-of-code/2023/5/src/main.rs
+++ advent-of-code/2023/5/src/main.rs
@@ -1,9 +1,21 @@
 
+#[derive(Copy, Clone, PartialEq)]
+struct Cluster {
+    start: u64,
+    range: u64,
+    has_been_mapped: bool
+}
+
+struct Mapping {
+    source: u64,
+    destination: u64,
+    range: u64
+}
+
 fn main() {
-    let mut seeds: Vec<&str> = Vec::new();
     let mut closest_distance = u64::MAX;
     let almanac_input = std::fs::read_to_string("data/input.txt").unwrap();
-    let seeds: Vec<&str> = almanac_input
+    let seeds_input: Vec<&str> = almanac_input
         .split("\n")
         .nth(0)
         .unwrap()
@@ -13,31 +25,112 @@ fn main() {
         .split(" ")
         .filter(|i| *i != "")
         .collect();
-    for seed in seeds {
-        let mut almanac_tracker: u64 = seed.parse().unwrap();
-        let mut value_in_almanac_section = false;
-        for (line_number, line) in almanac_input.split("\n").enumerate() {
-            if line_number == 0 {
-                continue;
-            } else if line.contains(":") {
-                value_in_almanac_section = false;
-            } else if line != "" {
-                let destination_range_start: u64 = line.split(" ").nth(0).unwrap().parse().unwrap();
-                let source_range_start: u64 = line.split(" ").nth(1).unwrap().parse().unwrap();
-                let range_length: u64 = line.split(" ").nth(2).unwrap().parse().unwrap();
-                // if almanac tracker is present in the source mapping, convert the value for the section
-                if !value_in_almanac_section {
-                    if almanac_tracker >= source_range_start && almanac_tracker < source_range_start + range_length {
-                        let increment = almanac_tracker - source_range_start;
-                        almanac_tracker = destination_range_start + increment;
-                        value_in_almanac_section = true;
-                    }
-                }
-            }
+    let mut seeds: Vec<Cluster> = Vec::new();
+    let mut previous_entry = "";
+
+    // compose initial clusters
+    for (iter, seed_entry) in seeds_input.into_iter().enumerate() {
+        if iter % 2 == 1 {
+            seeds.push(Cluster { start: previous_entry.parse().unwrap(), range: seed_entry.parse().unwrap(), has_been_mapped: false });
         }
-        if almanac_tracker < closest_distance {
-            closest_distance = almanac_tracker;
+        previous_entry = seed_entry;
+    }
+
+    // ingest almanac into data structure
+    let mut almanac: [Vec<Mapping>; 7] = [Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()];
+    let mut almanac_index = 0;
+    for line in almanac_input.split("\n") {
+        if line.contains(":") {
+            almanac_index = match line.split(" ").nth(0).unwrap() {
+                "seed-to-soil" => 0,
+                "soil-to-fertilizer" => 1,
+                "fertilizer-to-water" => 2,
+                "water-to-light" => 3,
+                "light-to-temperature" => 4,
+                "temperature-to-humidity" => 5,
+                "humidity-to-location" => 6,
+                _ => 7
+            }
+        } else if line != "" {
+            let destination: u64 =
+            line.split(" ").nth(0).unwrap().parse().unwrap();
+        let source: u64 = line.split(" ").nth(1).unwrap().parse().unwrap();
+        let range: u64 = line.split(" ").nth(2).unwrap().parse().unwrap();
+            almanac[almanac_index].push(Mapping { source, destination, range });
         }
     }
+
+    let mut clusters = seeds;
+    // for each section in the almanac
+    for section in almanac {
+        for cluster in &mut clusters {
+            cluster.has_been_mapped = false;
+        }
+        // while map_found_in_cluster_set
+        let mut map_found_in_cluster_set = true;
+        while map_found_in_cluster_set {
+            map_found_in_cluster_set = false;
+            // []new_clusters
+            let mut new_clusters: Vec<Cluster> = Vec::new();
+            // for each []cluster
+            for cluster in &clusters {
+                let mut cluster_contains_map = false;
+                // if cluster hasn't already been mapped (if it has already been mappped- push directly to []new_clusters)
+                if cluster.has_been_mapped {
+                    new_clusters.push(*cluster);
+                } else {
+                    // for each []mapping
+                    for map in &section {
+                        let map_lower_bound = map.source;
+                        let map_upper_bound = map.source + map.range;
+                        let cluster_lower_bound = cluster.start;
+                        let cluster_upper_bound = cluster.start + cluster.range;
+                        if map_lower_bound <= cluster_lower_bound && map_upper_bound >= cluster_upper_bound {
+                            map_found_in_cluster_set = true;
+                            cluster_contains_map = true;
+                            // map entire cluster and append to []new_clusters
+                            let offset = cluster_lower_bound - map_lower_bound;
+                            new_clusters.push(Cluster{start: map.destination + offset, range: cluster.range, has_been_mapped: true});
+                        } else if cluster_lower_bound < map_lower_bound && cluster_upper_bound > map_upper_bound {
+                            map_found_in_cluster_set = true;
+                            cluster_contains_map = true;
+                            // form three new clusters
+                            let offset = map_lower_bound - cluster_lower_bound;
+                            new_clusters.push(Cluster { start: cluster_lower_bound, range: offset, has_been_mapped: false });
+                            new_clusters.push(Cluster { start: map.destination, range: map.range, has_been_mapped: true });
+                            new_clusters.push(Cluster { start: map_upper_bound, range: cluster_upper_bound - map_upper_bound, has_been_mapped: false })
+                        } else if map_lower_bound <= cluster_lower_bound && map_upper_bound > cluster_lower_bound {
+                            map_found_in_cluster_set = true;
+                            cluster_contains_map = true;
+                            // form two new clusters (bottom one is mapped)
+                            let offset = cluster_lower_bound - map_lower_bound;
+                            new_clusters.push(Cluster { start: map.destination + offset, range: map_upper_bound - cluster_lower_bound, has_been_mapped: true });
+                            new_clusters.push(Cluster { start: map_upper_bound, range: cluster_upper_bound - map_upper_bound, has_been_mapped: false });
+                        } else if map_lower_bound < cluster_upper_bound && map_upper_bound >= cluster_upper_bound {
+                            map_found_in_cluster_set = true;
+                            cluster_contains_map = true;
+                            // form two new clusters (top one is mapped)
+                            new_clusters.push(Cluster { start: cluster_lower_bound, range: map_lower_bound - cluster_lower_bound, has_been_mapped: false });
+                            new_clusters.push(Cluster { start: map.destination, range: cluster_upper_bound - map_lower_bound, has_been_mapped: true });
+                        }
+                    }
+                }
+                if !cluster_contains_map {
+                    new_clusters.push(*cluster);
+                }
+            }
+            // dedup []new_clusters
+            new_clusters.dedup();
+            // set []cluster to []new_clusters
+            clusters = new_clusters;
+        }
+    }
+
+    for cluster in &clusters {
+        if cluster.start < closest_distance {
+            closest_distance = cluster.start;
+        }
+    }
+
     println!("Closest seed: {:#}", closest_distance);
 }
